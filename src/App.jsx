0dba071cce3d53bc86fd3e63a65621dd2106a064
szymptom <bpodkolzin@kse.org.ua>
2024-10-25 04:11:01 +0300

    initialize vite + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = function() {
+
+  return (
+    <>
+    <h1 className="bg-blue-300 my-12 py-4 rounded-lg font-medium text-2xl tracking-tight text-slate-900">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
