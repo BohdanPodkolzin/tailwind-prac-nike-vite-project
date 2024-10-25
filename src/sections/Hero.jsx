88427f51975b71bfd3c87f4c987dfb746fe2bd5b
szymptom <bpodkolzin@kse.org.ua>
2024-10-25 21:50:12 +0300

    button component & css for html hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants"
 
 const Hero = () => {
   return (
@@ -8,19 +9,34 @@ const Hero = () => {
       className="w-full flex lg:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div
-        className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"
-      >
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+        className="relative xl:w-2/5 flex flex-col 
+        justify-center items-start w-full max-xl:padding-x pt-28">
+        
+        <p className="max-sm:text-xl text-2xl font-montserrat text-coral-red">Our Summer Collection</p>
+        
+        <h1 className="mt-10 font-palanquin text-8xl font-bold max-sm:text-[68px] max-sm:leading-[80px]">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span>Nike </span>
-          Shoes
+          <span className="text-coral-red font-mono inline-block mt-5">Nike</span> Shoes
         </h1>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, laborum.</p>
-        <Button label="Show now" iconURL={arrowRight}/>
+        <p className="font-montserrat font-bold text-slate-700 text-lg leading-8 mt-6 mb-14">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, laborum.</p>
+
+        <Button label="Shop now" iconURL={arrowRight}/>
+        
+        <div className="flex justify-starts items-start
+         flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-semibold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-700 text-xl">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+
       </div>
+
+          
     </section>
   );
 };
