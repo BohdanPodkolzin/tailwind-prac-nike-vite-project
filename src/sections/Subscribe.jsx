075372a4be19fe6ca0c08fe83459e46fdfbc0a8c
szymptom <bpodkolzin@kse.org.ua>
2024-10-28 05:29:03 +0200

    implemented subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,10 +1,19 @@
-
+import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
-    <div>
+    <section className=" ustify-center max-container flex flex-col" id="contact-us">
+
+      <h3 className="w-full text-center capitalize text-4xl leading-[68px] font-semibold font-palanquin">sign for updates <span className="text-coral-red">Updates</span> & Newsletter</h3>
       
-    </div>
+      <div className="flex w-full max-sm:w-full sm:border sm:border-slate-700 p-3 mt-10 rounded-full">
+        <input className="input py-2 font-montserrat" type="email" placeholder="subscribe@nike.com" />
+        <div className="max-sm:ml-3 max-sm:w-[60%] flex p-0.4">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    
+    </section>
   )
 }
 
